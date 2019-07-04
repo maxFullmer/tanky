df4b1fade6737c9d2f43bbaab96ebebfd1404d9e
Max-F <max.fullmer@outlook.com>
2019-07-03 20:20:51 -0700

    Stage 0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      tankArmory: [],
+      id: 0,
+      tankName: "",
+      tankOwner: "",
+      pH: 7,
+      NH3: 0,
+      temperature: 68,
+      salinity: 0
+     };
+  }
+  render() {
+    return (
+      <div>
+        TANKY
+      </div>
+    );
+  }
 }
 
 export default App;
