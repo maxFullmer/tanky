4ab7ee843fdf2bf1dd9652a8fd2075a696aade81
Max-F <max.fullmer@outlook.com>
2019-07-05 13:27:47 -0700

    ReST operations

--- server/controllers/managementController.js
+++ server/controllers/managementController.js
@@ -1,32 +1,91 @@
-const tankData = require('../tankData.json');
-let id = tankData.length;
+const ownerData = require('../ownerData.json');
+//don't change these VVV
+
+let currentOwner = 1;
+let ownerID = 0;
+let tankID = 2;
+
 
 module.exports = {
-    read: (req, res, next) => {
-        res.status(200).send(tankData)
+
+    readOwnerData: (req, res, next) => {
+        res.status(200).send(ownerData);
     },
 
-    create: (req, res, next) => {
-        let {tankName, tankOwner} = req.body;
-        tankData.push({
-            id: id,
+    createOwner: (req, res, next) => {
+        ownerData.push({
+            owner: `default${ownerID + 1}`,
+            ownerID: ownerID + 1,
+            tankArmory: [
+                {
+                    tankID: tankID,
+                    tankName: `Tank ${tankID}`,
+                    ph: 7,
+                    NH3: 0,
+                    temperature: 68,
+                    salinity: 0
+                }
+            ]
+        })
+        tankID++;
+        ownerID++;
+        res.status(200).send(ownerData)
+},
+
+    getOwnerAfterNextClick: (req, res, next) => {
+        console.log(currentOwner)
+        if (currentOwner < ownerData.length) {
+            currentOwner++;
+        }
+        console.log(currentOwner)
+        res.status(200).send()
+    },
+
+    getOwnerAfterPrevClick: (req, res, next) => {
+        console.log(currentOwner)
+        if (currentOwner > 1) {
+            currentOwner--;
+        }
+        console.log(currentOwner)
+        res.status(200).send()
+    },
+
+    updateOwnerName: (req, res, next) => {
+        const newOwnerName = req.params.name;
+        ownerData[currentOwner-1].owner = `${newOwnerName}`;
+        res.status(200).send(ownerData);
+    },
+
+    deleteOwner: (req, res, next) => {
+        ownerData.splice(currentOwner-1,1);
+        res.status(200).send(ownerData);
+    },
+
+    createTank: (req, res, next) => {
+        const {tankName} = req.body;
+        
+        ownerData[currentOwner-1].tankArmory.push({
+            tankID: tankID,
             tankName,
-            tankOwner,
             ph: 7,
             NH3: 0,
             temperature: 68,
             salinity: 0
         });
-        id++;
-        res.status(200).send(tankData);
+        tankID++;
+        res.status(200).send(ownerData);
     },
 
+    updateTankName: (req, res, next) => {
+        const targetTankID = req.params.id;
+        const newTankName = req.params.newName;
+        ownerData[currentOwner-1].tankArmory[targetTankID].tankName = `${newTankName}`;
+        res.status(200).send(ownerData);
+    },
 
-
-    delete: (req, res, next) => {
-        const targetID = req.params.id;
-        // const tankIndex = tankData.findIndex(tank => tank.id == targetID);
-        tankData.splice(targetID,1);
-        res.status(200).send(tankData);
+    deleteTank: (req, res, next) => {
+        const targetTankID = req.params.id;
+        ownerData[currentOwner-1].tankArmory.splice(targetTankID,1);
+        res.status(200).send(ownerData);
     }
 }
